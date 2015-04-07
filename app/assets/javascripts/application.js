1b3928c9f303c56da1c39065a1c4199dc1b0e293
seyhaheng2 <seyhaheng2@gmail.com>
2015-04-07 23:30:24 +0700

    afds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,11 @@
 //= require react
 //= require react_ujs
 //= require components
-//= require jquery-latest.min
+//= require elementQuery.min
 //= require theme-scripts
 //= require lightbox
 //= require iscroll
-//= require modernizr.custom.50878
+//= require dat-menu
 //= require SmoothScroll.min
 //= require owl.carousel.min
 
